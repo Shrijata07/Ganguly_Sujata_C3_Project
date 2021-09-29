db2fe65b483a75f60841f074fdde4e04544de4e1
Shrijata07 <88281618+Shrijata07@users.noreply.github.com>
2021-09-29 11:07:21 +0530

    Update RestaurantTest.java

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -29,7 +29,6 @@ class RestaurantTest {
     //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
 
-    //need to fix
     @Test
     public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time() {
 
